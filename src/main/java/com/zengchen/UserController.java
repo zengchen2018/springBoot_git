3507d8eb8b52d0de084e830f9d58bd31909ef1be
zengchen <503104457@qq.com>
2018-11-08 14:30:31 +0800

    2222

---
+++ src/main/java/com/zengchen/UserController.java
@@ -0,0 +1,20 @@
+package com.zengchen;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: Zengchen
+ * @create: 2018-11-08 14:28
+ **/
+@RestController
+public class UserController {
+
+
+    @RequestMapping
+    public String findAll() {
+        return null;
+    }
+}
